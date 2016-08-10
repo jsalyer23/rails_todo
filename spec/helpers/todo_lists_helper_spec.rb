716dba7d1689cda3844414a049a2eb49cc80e5a9
Joshua Salyer <joshsalyer23@gmail.com>
2016-08-10 16:29:08 -0500

    Added todo_items show and new, wrote test that's closer to passing but still not, added format documentation option for rspec

--- spec/helpers/todo_lists_helper_spec.rb
+++ spec/helpers/todo_lists_helper_spec.rb
@@ -10,6 +10,6 @@ require 'spec_helper'
 #     end
 #   end
 # end
-describe TodoListsHelper do
-  skip "add some examples to (or delete) #{__FILE__}"
-end
+# describe TodoListsHelper do
+#   skip "add some examples to (or delete) #{__FILE__}"
+# end
--- spec/views/todo_items/index.html.erb_spec.rb
+++ spec/views/todo_items/index.html.erb_spec.rb
@@ -1,5 +1,5 @@
-require 'rails_helper'
+require 'spec_helper'
 
-RSpec.describe "todo_items/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+# RSpec.describe "todo_items/index.html.erb", type: :view do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
